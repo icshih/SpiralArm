c54e3713d3bf002be9fd8f9aa86b0101184b5d53
I-chun SHIH <icshih@mac.com>
2017-12-01 11:39:06 +0100

    update

--- py/tap/tap_query.py
+++ py/tap/tap_query.py
@@ -14,8 +14,8 @@ def db_connect():
 
 
 def db_create_table(conn):
-    cur = conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS gaia_ucac4_colour ('
+    create = conn.cursor()
+    create.execute('CREATE TABLE IF NOT EXISTS gaia_ucac4_colour ('
                 'gaia_source_id bigint NOT NULL, '
                 'l double precision,'
                 'b double precision,'
@@ -23,14 +23,30 @@ def db_create_table(conn):
                 'dec double precision,'
                 'pmra double precision,'
                 'pmdec double precision,'
+                'parallax double precision,'
+                'phot_g_mean_mag double precision,'
+                'phot_g_mean_flux double precision,'
                 'ucac4_id text NOT NULL,'
                 'b_mag float,'
                 'v_mag float);')
     conn.commit()
 
+def get_and_ingest(constraint):
+    result = Vizier.query_constraints(catalog='I/322A/out', UCAC4=constraint)
+    data = result[0]
+    for i in np.arange(len(data)):
+        ucac4 = data.field('UCAC4')[i]
+        bmag = float(data.field('Bmag')[i])
+        vmag = float(data.field('Vmag')[i])
+        g_tuple = id_dict[ucac4]
+        insert.execute(
+            'INSERT INTO gaia_ucac4_colour (gaia_source_id, l, b, ra, dec, pmra, pmdec, parallax, phot_g_mean_mag, phot_g_mean_flux, ucac4_id, b_mag, v_mag) '
+            'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);',
+            (g_tuple[0], g_tuple[1], g_tuple[2], g_tuple[3], g_tuple[4], g_tuple[5], g_tuple[6], g_tuple[7], g_tuple[8],g_tuple[9], ucac4, bmag, vmag))
+    conn_.commit()
 
 # alternative query
-query = 'SELECT g.source_id, g.l, g.b, g.ra, g.dec, g.pmra, g.pmdec, u.ucac4_id ' \
+query = 'SELECT g.source_id, g.l, g.b, g.ra, g.dec, g.pmra, g.pmdec, g.parallax, g.phot_g_mean_mag, g.phot_g_mean_flux, u.ucac4_id ' \
         'FROM (' \
         'SELECT source_id, original_ext_source_id AS ucac4_id ' \
         'FROM gaiadr1.ucac4_best_neighbour ' \
@@ -52,7 +68,9 @@ job = Gaia.launch_job_async(query, output_file='/Users/icshih/Documents/Research
                                                'gaia_source_xm_ucac4.vot', dump_to_file=True)
 data = job.get_results()
 
+# using constraint in VizieR, http://vizier.cfa.harvard.edu/vizier/vizHelp/cst.htx#char
 conn_ = db_connect()
+db_create_table(conn_)
 insert = conn_.cursor()
 count = 0
 constraint = '<<'
@@ -67,25 +85,16 @@ for d in data:
     dec = f['dec'].item(0)
     pmra = f['pmra'].item(0)
     pmdec = f['pmdec'].item(0)
-    id_dict[u_id] = g_id, l, b, ra, dec, pmra, pmdec
+    parallax = f['parallax'].item(0)
+    gmag = f['phot_g_mean_mag'].item(0)
+    gflux = f['phot_g_mean_flux'].item(0)
+    id_dict[u_id] = g_id, l, b, ra, dec, pmra, pmdec, parallax, gmag, gflux
     constraint = constraint + ';' + u_id
     count = count + 1
     if (count >= 500):
-        result = Vizier.query_constraints(catalog='I/322A/out', UCAC4=constraint)
-        data = result[0]
-        for i in np.arange(len(data)):
-            ucac4 = data.field('UCAC4')[i]
-            bmag = float(data.field('Bmag')[i])
-            vmag = float(data.field('Vmag')[i])
-            g_tuple = id_dict[ucac4]
-            insert.execute(
-                'INSERT INTO gaia_ucac4_colour (gaia_source_id, l, b, ra, dec, pmra, pmdec, ucac4_id, b_mag, v_mag) '
-                'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);',
-                (g_tuple[0], g_tuple[1], g_tuple[2], g_tuple[3], g_tuple[4], g_tuple[5], g_tuple[6], ucac4, bmag, vmag))
-        conn_.commit()
+        get_and_ingest(constraint)
         count = 0
         constraint = '<<'
         id_dict.clear()
 
-
-conn_.commit()
+get_and_ingest(constraint)
