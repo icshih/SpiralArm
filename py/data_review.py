9261de0153b8a4fd86c06a90ab2aa2d1cffe0534
I-chun SHIH <icshih@mac.com>
2017-12-12 15:15:43 +0100

    update figure

--- py/data_review.py
+++ py/data_review.py
@@ -1,8 +1,10 @@
 import astropy.units as u
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 import psycopg2
 import astropy.coordinates as coord
+
 
 def db_connect():
     URI = 'postgresql://{}@{}:{}/{}'.format('postgres', 'localhost', '10000', 'postgres')
@@ -21,17 +23,30 @@ def sky_distribution(conn):
     l = np.array(l)
     b = np.array(b)
     gal = coord.SkyCoord(l=l, b=b, unit=(u.deg, u.deg), frame='galactic')
-    fig = plt.figure(figsize=(16,9))
+    fig = plt.figure(figsize=(16, 9))
     ax1 = fig.add_subplot(111, projection='aitoff')
     ax1.set_title('Object Distribution ({:,})'.format(l.size))
     ax1.set_xlabel('Galactic Lon.')
     ax1.set_ylabel('Galactic Lat.')
-    ax1.scatter(gal.l.wrap_at(180*u.deg).radian, gal.b.radian, s=0.1)
+    ax1.scatter(gal.l.wrap_at(180 * u.deg).radian, gal.b.radian, s=0.1)
     ax1.grid(True)
     plt.show()
 
 
-def data_distribution(data):
+def data_distribution(conn):
+    cur = conn.cursor()
+    p = list()
+    g = list()
+    b = list()
+    v = list()
+    cur.execute(
+        'SELECT parallax, phot_g_mean_mag, b_mag, v_mag FROM gaia_ucac4_colour WHERE b_mag != \'nan\' AND v_mag != \'nan\'')
+    for record in cur:
+        p.append(record[0])
+        g.append(record[1])
+        b.append(record[2])
+        v.append(record[3])
+    data = np.array([p, g, b, v])
     fig = plt.figure(figsize=(12, 12))
 
     ax1 = fig.add_subplot(221)
@@ -59,6 +74,7 @@ def data_distribution(data):
     ax4.hist(data[3])
     plt.show()
 
+
 def data_correlation(data):
     fig = plt.figure(figsize=(16, 16))
     ax1 = fig.add_subplot(121)
@@ -74,40 +90,33 @@ def data_correlation(data):
     plt.show()
 
 
-def get_data(conn):
+def parallax_distribution(conn):
     cur = conn.cursor()
     p = list()
     g = list()
-    b = list()
-    v = list()
-    cur.execute('SELECT parallax, phot_g_mean_mag, b_mag, v_mag FROM gaia_ucac4_colour WHERE b_mag != \'nan\' AND v_mag != \'nan\'')
+    pe = list()
+    cur.execute(
+        'SELECT parallax, parallax_error, phot_g_mean_mag FROM gaia_ucac4_colour WHERE parallax > 0 AND b_mag != \'nan\' AND v_mag != \'nan\'')
     for record in cur:
         p.append(record[0])
-        g.append(record[1])
-        b.append(record[2])
-        v.append(record[3])
-    data = np.array([p, g, b ,v])
-    return data
+        pe.append(record[1])
+        g.append(record[2])
+    data = np.array([p, pe, g])
+    fig = plt.figure(figsize=(12, 12))
+    ax1 = fig.add_subplot(111)
+    ax1.set_xlabel('parallax (mas/y)')
+    ax1.set_ylabel('fractional parallax error')
+
+    colr = cm.afmhot
+    norm = cm.colors.Normalize(vmin=np.min(data[2]), vmax=np.max(data[2]))
+    dist = ax1.scatter(data[0], data[1] / data[0], c=data[2], cmap=colr, norm=norm, s=0.3)
+    cb = fig.colorbar(dist, orientation='vertical')
+    cb.set_label('Phot G mean mag')
+    plt.show()
+
 
 conn_ = db_connect()
+
 # sky_distribution(conn_)
-output = get_data(conn_)
-# data_distribution(output)
-data_correlation(output)
-
-
-
-
-
-
-
-# gala = coord.Galactic(l*u.degree, b*u.degree)
-#
-# fig = plt.figure(figsize=(8,6))
-#
-# ax = fig.add_subplot(111, projection="mollweide")
-#
-# ll = coord.Angle(gala.l)
-# bb = coord.Angle(gala.b)
-# ax.scatter(ll.radian, bb.radian)
-
+# data_distribution(conn_)
+parallax_distribution(conn_)
