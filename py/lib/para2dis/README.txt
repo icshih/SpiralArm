1b9bf8c2d014421bf9687796e9e2f401030bf5f9
I-chun SHIH <icshih@mac.com>
2018-02-06 10:28:12 +0100

    prepare library

---
+++ py/lib/para2dis/README.txt
@@ -0,0 +1,1 @@
+Calculate distance from measured parallax
---
+++ py/lib/para2dis/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='para2dis',
+    packages=['para2dis'],
+    version='0.1',
+    author='I-Chun SHIH',
+    author_email='icshih@mac.com',
+    url='https://github.com/icshih/SpiralArm.git',
+    keywords = ['astronomy', 'astrometry'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ],
+    description='Measured parallax to distance distribution'
+)
