b327f72572c29a6263a2356329141a6b3ec44b9f
icshih <icshih@mac.com>
2018-02-20 14:52:03 +0100

    update cross match table

--- py/sa_cross_match.py
+++ py/sa_cross_match.py
@@ -8,7 +8,10 @@ import psycopg2
 from astroquery.gaia import Gaia
 from astroquery.vizier import Vizier
 
-query = 'SELECT g.source_id, g.l, g.b, g.ra, g.dec, g.pmra, g.pmdec, g.parallax, g.parallax_error, g.phot_g_mean_mag, g.phot_g_mean_flux, u.ucac4_id ' \
+cross_match_table = 'gaia_ucac4'
+
+query = 'SELECT g.source_id, g.l, g.b, g.ra, g.ra_error, g.dec, g.dec_error, g.pmra, g.pmra_error,' \
+        ' g.pmdec, g.pmdec_error, g.parallax, g.parallax_error, g.phot_g_mean_mag, g.phot_g_mean_mag_error, u.ucac4_id ' \
         'FROM (' \
         'SELECT source_id, original_ext_source_id AS ucac4_id ' \
         'FROM gaiadr1.ucac4_best_neighbour ' \
@@ -29,21 +32,25 @@ def db_connect(host, port, db_name, user, password):
 def db_create_table(connection):
     """Create Gaia - External Catalogue cross-matching table"""
     create = connection.cursor()
-    create.execute('CREATE TABLE IF NOT EXISTS gaia_ucac4_colour ('
+    create.execute('CREATE TABLE IF NOT EXISTS {0} ('
                    'gaia_source_id bigint NOT NULL, '
                    'l double precision,'
                    'b double precision,'
                    'ra double precision,'
+                   'ra_error double precision,'
                    'dec double precision,'
+                   'dec_error double precision,'
                    'pmra double precision,'
+                   'pmra_error double precision,'
                    'pmdec double precision,'
+                   'pmdec_error double precision,'
                    'parallax double precision,'
                    'parallax_error double precision,'
                    'phot_g_mean_mag double precision,'
-                   'phot_g_mean_flux double precision,'
+                   'phot_g_mean_error double precision,'
                    'ucac4_id text NOT NULL,'
                    'b_mag float,'
-                   'v_mag float);')
+                   'v_mag float);'.format(cross_match_table))
     connection.commit()
 
 
@@ -57,11 +64,13 @@ def get_and_ingest(connection, id_dict, constraint):
         vmag = float(dataset.field('Vmag')[i])
         g_tuple = id_dict[ucac4]
         insert.execute(
-            'INSERT INTO gaia_ucac4_colour (gaia_source_id, l, b, ra, dec, pmra, pmdec, parallax, parallax_error, phot_g_mean_mag, phot_g_mean_flux, ucac4_id, b_mag, v_mag) '
-            'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);',
+            'INSERT INTO gaia_ucac4_colour (gaia_source_id, l, b, ra, ra_error, dec, dec_error, pmra, pmra_error, '
+            'pmdec, pmdec_error, parallax, parallax_error, phot_g_mean_mag, phot_g_mean_error, ucac4_id, b_mag, v_mag) '
+            'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);',
             (g_tuple[0], g_tuple[1], g_tuple[2], g_tuple[3], g_tuple[4], g_tuple[5], g_tuple[6], g_tuple[7], g_tuple[8],
-             g_tuple[9], g_tuple[10], ucac4, bmag, vmag))
+             g_tuple[9], g_tuple[10], g_tuple[11], g_tuple[12], g_tuple[13], g_tuple[13], g_tuple[14], ucac4, bmag, vmag))
     connection.commit()
+    insert.close()
 
 
 def process(connection, dataset):
@@ -75,14 +84,18 @@ def process(connection, dataset):
         l = f['l'].item(0)
         b = f['b'].item(0)
         ra = f['ra'].item(0)
+        ra_error = f['ra_error'].item(0)
         dec = f['dec'].item(0)
+        dec_error = f['dec_error'].item(0)
         pmra = f['pmra'].item(0)
+        pmra_error = f['pmra_error'].item(0)
         pmdec = f['pmdec'].item(0)
+        pmdec_error = f['pmdec_error'].item(0)
         parallax = f['parallax'].item(0)
         parallax_error = f['parallax_error'].item(0)
         gmag = f['phot_g_mean_mag'].item(0)
-        gflux = f['phot_g_mean_flux'].item(0)
-        id_dict[u_id] = g_id, l, b, ra, dec, pmra, pmdec, parallax, parallax_error, gmag, gflux
+        gmag_error = f['phot_g_mean_error'].item(0)
+        id_dict[u_id] = g_id, l, b, ra, ra_error, dec, dec_error, pmra, pmra_error, pmdec, pmdec_error, parallax, parallax_error, gmag, gmag_error
         constraint = constraint + ';' + u_id
         count = count + 1
         if count >= 500:
